aaac7ca14201ae6491caa2d1a38bfd96ac16dec7
Karudan <k.karudan@gmail.com>
2024-06-29 12:26:13 +0530

    3Test

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,5 @@
-import pandas as pd
 
 def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
+    d = {'col1': [1,2], 'col2': [3,4]}    
+    print(d)
     print('Done x1.2')
